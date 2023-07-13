85c62c36881f36e725991dfd30e5758bbd9bedb1
omar khaled <mer27320@gmail.com>
2023-07-14 01:49:28 +0300

    omar khaled

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("a    a^2    a^3");
+        System.out.print("1    1      1");
+        System.out.print("2    4      8");
+        System.out.print("3    9      27");
+        System.out.print("4    16     64");
+
+    }
+}
